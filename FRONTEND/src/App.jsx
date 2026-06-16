9ae61f780089d289b822c8a0decd6feb14c73ff5
Mudassir Ali <muddasira435@gmail.com>
2026-06-17 03:36:26 +0500

    Setup the react project using vite and developer the header and make the structure of header using colors. Apply responsiveness and continue working on structure of website

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Header from './header.jsx'
+function App() {
+  
+
+  return (
+    <>
+    <div className="grid lg:grid-cols-[0.8fr_3.2fr]  gap-0">
+      <Header/>
+      <div className="border-2 border-black h-[1200px]">
+
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
