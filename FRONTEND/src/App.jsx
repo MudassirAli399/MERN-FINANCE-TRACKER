95e0dcde15b076f6eeb0527994aa873eeeefc1a0
Mudassir Ali <muddasira435@gmail.com>
2026-06-18 01:06:13 +0500

    remove duplicate data and also remember first i am using static data to read structure of website

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,21 +1,4 @@
 
-import Header from './header.jsx'
-function App() {
-  
-
-  return (
-    <>
-    <div className="grid lg:grid-cols-[0.8fr_3.2fr]  gap-0">
-      <Header/>
-      <div className="border-2 border-black h-[1200px]">
-
-      </div>
-    </div>
-    </>
-  )
-}
-
-export default App
 
 import Header from './header.jsx'
 import Dashboard from './Pages/dashboard.jsx'
